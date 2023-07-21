ed3df8107ec028f2304d6bcedc14f54ff586055e
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2023-07-21 01:48:48 -0300

    change background style

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
 <div class="container">
-  <img src="./cat.png">
+  <div class="color-picker-container">
+    <input type="range" min="0" max="359" value="180" id="color-picker">
+  </div>
+  <img src="./astronauta.gif">
   <input type="text" id="input-field" placeholder="Bread">
-  <button id="add-button">Add to cart</button>
+  <button id="add-button">ADD TO CART</button>
   <ul id="products-list"></ul>
 </div>
 `
---
+++ public/fonts/readme.txt
@@ -0,0 +1,8 @@
+This is Steve, a typeface designed by Vince Whitlock
+and released in 1983. Until now the typeface has not been
+digitized, so I did my best to do so. I have provided
+the original Steve, along with a version called Steven,
+which has sharp corners.
+
+link:
+https://www.1001fonts.com/steve-font.html
