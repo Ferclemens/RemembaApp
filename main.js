921e4b7f369f6668146a329fce9c26db98c56e03
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2023-07-25 14:15:51 -0300

    add github and linkedin links

--- main.js
+++ main.js
@@ -13,7 +13,11 @@ document.querySelector('#app').innerHTML = `
   <input type="text" id="input-field" placeholder="Oxygen...?" autofocus>
   <button id="add-button">add to list</button>
   <ul id="products-list"></ul>
-  <button id="clean-button">clean</button>
+  <div class="buttons-group">
+    <a href="https://github.com/Ferclemens" target="_blank"><img src="./githubPixelLogo.png"></a>
+    <a href="https://www.linkedin.com/in/foclemens/" target="_blank"><img src="./linkedinPixelLogo.svg"></a>
+    <button id="clean-button">clean</button>
+  </div>
 </div>
 `
 app()
