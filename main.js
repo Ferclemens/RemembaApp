0c6eaf3bfea5a0e7ac6a6d6c4260ce09f4e784f3
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2023-07-25 14:49:20 -0300

    :mag: fix SEO performance

--- main.js
+++ main.js
@@ -8,16 +8,16 @@ document.querySelector('#app').innerHTML = `
     <input type="range" min="1" max="360" value="1" id="color-picker">
   </div>
   <h1 class="text-up">What do you <br> need?</h1>
-  <img src="./astronauta.gif">
+  <img src="./astronauta.gif" alt="astronaut spinning in space">
   <h1 class="text-down">for your next <br> adventure</h1>
+  <div class="buttons-group">
+    <a href="https://github.com/Ferclemens" target="_blank"><img src="./githubPixelLogo.png" alt="Github pixel art logo"></a>
+    <a href="https://www.linkedin.com/in/foclemens/" target="_blank"><img src="./linkedinPixelLogo.svg" alt="LinkedIn pixel art logo"></a>
+  </div>
   <input type="text" id="input-field" placeholder="Oxygen...?" autofocus>
   <button id="add-button">add to list</button>
   <ul id="products-list"></ul>
-  <div class="buttons-group">
-    <a href="https://github.com/Ferclemens" target="_blank"><img src="./githubPixelLogo.png"></a>
-    <a href="https://www.linkedin.com/in/foclemens/" target="_blank"><img src="./linkedinPixelLogo.svg"></a>
     <button id="clean-button">clean</button>
-  </div>
 </div>
 `
 app()
