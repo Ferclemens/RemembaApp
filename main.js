b42a782eadbbe3e007aa46f248d6687ee4d6d620
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2023-08-07 23:47:54 -0300

    :bug: fix instructions flex style

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
 <div class="container">
+  <div class="instructions" id="instructions">
+    <h3 class="instructions-title">Instructions<h3>
+    <p>- Write anything</p>
+    <p>- Add to list with the button</p>
+    <p>- Remember what you need </p>
+    <p>- You can delete items one by one clicking it, or clear the list with "clean" button</p>
+  </div>
   <div class="color-picker-container">
     <input type="range" min="1" max="360" value="1" id="color-picker">
   </div>
@@ -20,13 +27,14 @@ document.querySelector('#app').innerHTML = `
     <button id="clean-button">clean</button>
     <button id="instructions-button">instructions</button>
 </div>
-<div class="instructions" id="instructions">
-  <h3 class="instructions-title">Instructions<h3>
-  <p>- Write anything</p>
-  <p>- Add to list with the button</p>
-  <p>- Remember what you need </p>
-  <p>- You can delete items one by one clicking it, or clear the list with "clean" button</p>
-<div>
+
+<div class="instructions" id="clear-warning">
+  <p>Are you sure?<br>This action cannot be going back.</p>
+  <div class="button-group">
+    <button id="accept-clear">Accept</button>
+    <button id="cancel-clear">Cancel</button>
+  </div>
+</div>
 `
 console.log(typeof(null))
 app()
